361ae9c0c4074e0a8e8db108dec3ccd77316082b
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-19 11:14:34 +0530

    Basics of OOPS file pushed

---
+++ OOPSTutorialCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPSTutorialCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome To OOPS in C# Tutorial!");
+            Console.WriteLine("This is the main entry point of the program.");
+            Console.WriteLine("Stay tuned for more examples and concepts ahead.");
+        }
+    }
+}
