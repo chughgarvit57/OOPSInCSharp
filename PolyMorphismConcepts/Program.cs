d67531abc78d7293c247643dcf81be6b63f7ac19
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-20 14:27:48 +0530

    Polymorphism code

---
+++ PolyMorphismConcepts/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PolyMorphismConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
